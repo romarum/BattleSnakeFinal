369bd30f52f4dd17db2fd7ecb8e999f9d009a1ee
romarum <romarum@yahoo.ca>
2018-03-02 21:56:54 -0800

    safetyAroundBorders Removed

--- app/main.py
+++ app/main.py
@@ -107,7 +107,7 @@ def createGoals():
         print('Foodeater mode initiated')
         print('GOALS ', goals)
         safetyAroundSnakeHead()
-        safetyAroundBorders()
+        #safetyAroundBorders()
         for food in foods:
             print('Check food vs grid ', grid[food['x']][food['y']])
             if(int(grid[food['x']][food['y']]) == 0):
@@ -141,7 +141,7 @@ def createGoals():
     elif mode == 'killer':
         print('Killer mode initiated')
         print('GOALS ', goals)
-        safetyAroundBorders()
+        #safetyAroundBorders()
         for otherSnake in otherSnakes:
             if(int((otherSnake)['length']) < myLength):
                 try:
